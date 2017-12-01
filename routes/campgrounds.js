33035c73e8b4571d6e63698a6df1c4b4398359c3
Ivan <anz17n@gmail.com>
2017-12-01 03:12:55 +0000

    more but fixes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -101,7 +101,7 @@ router.delete('/:id', middleware.checkCampgroundOwnership, function(req, res) {
            req.flash('error', err.message);
            res.redirect('/campgrounds');
        } else {
-           req.flash('success', 'Campground edited');
+           req.flash('success', 'Campground deleted');
            // redirect
            res.redirect('/campgrounds/');
        }
